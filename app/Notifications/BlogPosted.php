9e64d40d191474cd89b7f1036f072c8f6067c8e3
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-08-08 07:57:19 +0800

    comment

--- app/Notifications/BlogPosted.php
+++ app/Notifications/BlogPosted.php
@@ -13,23 +13,41 @@ class BlogPosted extends Notification
 
     protected $blog;
 
+    /**
+     * Create a new notification instance.
+     *
+     * @param  mixed  $blog
+     * @return void
+     */
     public function __construct($blog)
     {
-        $this->blog = $blog;
+        $this->blog = $blog;  // Store the blog instance
     }
 
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail'];  // Specify that the notification will be sent via email
     }
 
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject('New Blog Post Created')
-            ->line('A new blog post has been created.')
-            ->line('Title: ' . $this->blog->title)
-            ->line('Content: ' . $this->blog->content)
-            ->action('View Blog Post', url('/blogs/' . $this->blog->id));
+            ->subject('New Blog Post Created')  
+            ->line('A new blog post has been created.')  
+            ->line('Title: ' . $this->blog->title) 
+            ->line('Content: ' . $this->blog->content) 
+            ->action('View Blog Post', url('/blogs/' . $this->blog->id));  
     }
 }
